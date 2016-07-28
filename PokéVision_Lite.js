61af11c8378f4753c921fb51f8ff57ee75d37e1b
Jason Yu <jasonycw1992@gmail.com>
2016-07-29 00:32:50 +0800

    [Not complete] Try to make one click bookmarklet for it

---
+++ PokéVision_Lite.js
@@ -0,0 +1,55 @@
+javascript:(
+	function(){
+		window.open('https://pokevision.com/');
+		window.popup=window.open('https://pokevision.com/');
+		window.popup.onload=function(){
+			$('.ad-unit').remove();
+			$('.home-sidebar').remove();
+			$('.leaflet-control-attribution').remove();
+			$('.ocrext-element.ocrext-wrapper').remove();
+			$('footer').remove();
+
+			$('.header').css('padding',"10px 0");
+			$('.header-mobile').css('position',"absolute");
+			$('.header-mobile').css('width',"100%");
+			$('.home').addClass('embed-map');
+			$('.home').attr('style', 'height: 100% !important; padding: 0');
+			$('.home-map').css('padding',"0");
+			$('.home-map').css('height',"100vh");
+			$('.home-map-scan').css('bottom','16px');
+			$('.home-map-scan').css('left','168px');
+			$('.bootstrap-select.show-tick').css('top','auto');
+			$('.bootstrap-select.show-tick').css('bottom','16px');
+			$('.bootstrap-select.show-tick').css('right','70px');
+
+			App.init();
+			App.header.init();
+
+			if (navigator.geolocation) {
+			    navigator.geolocation.getCurrentPosition(function(e) {
+			        var t = e.coords.latitude
+			          , o = e.coords.longitude;
+			        App.home.latitude = t;
+			        App.home.longitude = o;
+			        App.home.map.panTo({
+			            lat: t,
+			            lng: o
+			        });
+			        App.home.markers.center.setLatLng({
+			            lat: t,
+			            lng: o
+			        });
+			        App.home.updateMarkers();
+			        App.home.findNearbyPokemon(t, o);
+			        window.location.hash = '#/@' + t + ',' + o
+			    }, function(e) {
+			        App.error(e.message)
+			    })
+			} else {
+			    App.error('Your browser doesn\'t support location tracking, sorry!')
+			}
+
+			App.home.map.invalidateSize(new Object({debounceMoveend: true}));
+		};
+	}
+)();
